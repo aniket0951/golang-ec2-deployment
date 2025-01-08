2a5e2c0929feef550d7a3742ca370e9c81a03373
aniket suryawanshi <54351856+aniket0951@users.noreply.github.com>
2025-01-08 09:50:50 -0800

    Update main.go

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "application/json")
 
-		json.NewEncoder(w).Encode("function run success 1cr")
+		json.NewEncoder(w).Encode("function run success 100cr")
 	})
 	log.Println("Application up..")
 	err := http.ListenAndServe(":9090", nil)
